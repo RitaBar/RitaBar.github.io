5a086a55ee89113db08f801afda4144655dcfe95
RitaBar <bar17055@byui.edu>
2019-10-12 22:03:55 -0400

    updated

---
+++ lesson5/js/bannerad.js
@@ -0,0 +1,9 @@
+var today = new Date();
+
+var day = {weekday};
+var ad = "Saturday = Preston Pancakes in the Park! <br> 9:00 a.m. Saturday at the city park pavilion."
+
+
+document.getElementsByClassName('banner').innerHTML = "Saturday = Preston Pancakes in the Park! <br> 9:00 a.m. Saturday at the city park pavilion.";
+
+
