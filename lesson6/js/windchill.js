026e3b5830083ee4183796730e5c0576f043580e
RitaBar <bar17055@byui.edu>
2019-10-18 15:22:02 -0400

    updated

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,16 @@
+var temp = document.getElementById('highTemp');
+var speed = document.getElementById('windSpeed');
+var windchill = "NA";
+// change strings to numbers
+// do numbers meet criteria for offical requirements -- if statement
+// if numbers meet criteria is true {
+//     windchill = calculateWindChill(temp speed) + " " + &#8457};
+//     document.getElementById('wChill').textContent= windchill;
+//
+
+function calculateWindChill(temp, speed) {
+    var windchill;
+// do the math Math.
+    return windchill;
+    //35.74+0.6215t-35.75s^0.16+0.4275ts^0.16
+}
