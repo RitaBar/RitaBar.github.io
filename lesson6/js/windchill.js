9e841b67619379a944afa4d8724e3e699d1a6434
RitaBar <bar17055@byui.edu>
2019-10-23 23:15:08 -0400

    simplify windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,15 +1,10 @@
-var temp = parseFloat(document.getElementById('highTemp').innerHTML);
-var speed = parseFloat(document.getElementById('windSpeed').innerHTML);
+var temp = parseFloat(document.getElementById('highTemp').textContent);
+var speed = parseFloat(document.getElementById('windSpeed').textContent);
 
-var windchill = calculateWindChill(temp, speed);
+windchill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));
 
 if (windchill <= 50 && speed > 3) {
     document.getElementById('wChill').textContent = windchill.toFixed(0) + "\xB0F";
 } else {
     document.getElementById('wChill').textContent = "NA";
-}
-
-function calculateWindChill(temp, speed) {
-    windchill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));
-    return windchill;
 }
