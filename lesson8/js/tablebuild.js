c70847e4839207c00dcdc499a56c21bde4bcebec
RitaBar <bar17055@byui.edu>
2019-11-05 06:13:56 -0500

    update medium  background

--- lesson8/js/tablebuild.js
+++
@@ -1,8 +0,0 @@
-var today = new Date();
-
-var day = today.getDate();
-var month = today.getMonth() + 1;
-var year = today.getFullYear();
-
-document.getElementById('currentDate').textContent =  month + "." + day + "." + year;
-
