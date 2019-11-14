933d517dee2d2f3491a9109237b217ccbf61ea92
RitaBar <bar17055@byui.edu>
2019-11-14 15:02:29 -0500

    update html, css, js add img

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -5,7 +5,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
 
     const towns = jsonObject['towns'];
 
@@ -26,7 +26,7 @@ fetch(requestURL)
         p2.textContent = "Year Founded: " + towns[i].yearFounded;
         p3.textContent = "Current Population: " + towns[i].currentPopulation;
         p4.textContent = "Average Rainfall: " + towns[i].averageRainfall + " inches";
-        image.setAttribute('src', towns[i].photo);
+        image.setAttribute('src', "images/" + towns[i].photo);
         image.setAttribute('alt', "photo of "+ towns[i].name);
         
         town.appendChild(h2);
