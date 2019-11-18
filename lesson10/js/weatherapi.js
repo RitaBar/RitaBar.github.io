83252fb1f9505217cd7bae82640277377f316678
RitaBar <bar17055@byui.edu>
2019-11-18 17:55:17 -0500

    update calcWindChill function position

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,15 +1,3 @@
-function calcWindChill() {
-  var temp = parseFloat(document.getElementById('currentTemp').textContent);
-  var speed = parseFloat(document.getElementById('windSpeed').textContent);
-
-  windchill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));
-
-  if (windchill <= 50 && speed > 3) {
-    document.getElementById('wChill').textContent = windchill.toFixed(0) + "\xB0F";
-  } else {
-    document.getElementById('wChill').textContent = "NA";
-  }
-}
 
 const weatherObject = new XMLHttpRequest();
 weatherObject.open ("GET","//api.openweathermap.org/data/2.5/weather?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1", true );
@@ -17,6 +5,20 @@ weatherObject.send();
 weatherObject.onload = function() {
   let weatherInfo = JSON.parse(weatherObject.responseText);
   console.log(weatherInfo);
+
+  function calcWindChill() {
+    var temp = parseFloat(document.getElementById('currentTemp').textContent);
+    var speed = parseFloat(document.getElementById('windSpeed').textContent);
+  
+    windchill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));
+  
+    if (windchill <= 50 && speed > 3) {
+      document.getElementById('wChill').textContent = windchill.toFixed(0) + "\xB0F";
+    } else {
+      document.getElementById('wChill').textContent = "NA";
+    }
+  }
+  
 
   document.getElementById('currentCondition').innerHTML = weatherInfo.weather[0].main;
   document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
