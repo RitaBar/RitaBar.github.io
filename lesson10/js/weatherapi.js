79f32c30a1b83214cd4da5907d9bdab032af8ebb
RitaBar <bar17055@byui.edu>
2019-11-18 16:51:20 -0500

    update weatherapi.js from video B

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,16 @@
-const currentWeatherApiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1";
+const weatherObject = new HXLHttpRequest();
+weatherObject.open ("GET","//api.openweathermap.org/data/2.5/weather?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1", true )
+
+weatherObject.send();
+weatherObject.onload = function() {
+  let weatherInfo = JSON.parse(weatherObject.responseText);
+  console.log(weatherInfo);
+  
+} // end of onload
+
+
+
+/*const currentWeatherApiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1";
 const forecastApiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1";
 const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 
