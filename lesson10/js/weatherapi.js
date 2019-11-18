bbb01555d624df7a19c20610a51b85f3bde244fa
RitaBar <bar17055@byui.edu>
2019-11-18 17:10:13 -0500

    correct weatherapi.js 2

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const weatherObject = new HXLHttpRequest();
+const weatherObject = new XMLHttpRequest();
 weatherObject.open ("GET","//api.openweathermap.org/data/2.5/weather?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1", true );
 weatherObject.send();
 weatherObject.onload = function() {
