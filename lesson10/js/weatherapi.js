4b7d6e31e4813d0bafa730ec3c30790a157bd286
RitaBar <bar17055@byui.edu>
2019-11-18 17:51:11 -0500

    update weatherapi.js add calcWindChill function

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,3 +1,16 @@
+function calcWindChill() {
+  var temp = parseFloat(document.getElementById('currentTemp').textContent);
+  var speed = parseFloat(document.getElementById('windSpeed').textContent);
+
+  windchill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));
+
+  if (windchill <= 50 && speed > 3) {
+    document.getElementById('wChill').textContent = windchill.toFixed(0) + "\xB0F";
+  } else {
+    document.getElementById('wChill').textContent = "NA";
+  }
+}
+
 const weatherObject = new XMLHttpRequest();
 weatherObject.open ("GET","//api.openweathermap.org/data/2.5/weather?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1", true );
 weatherObject.send();
