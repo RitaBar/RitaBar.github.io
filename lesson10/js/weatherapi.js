f927ebb96821311cebbcea4d283438fccbff0281
RitaBar <bar17055@byui.edu>
2019-11-19 21:09:07 -0500

    update weatherapi.js forecast 3

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -33,7 +33,7 @@ const forecastObject = new XMLHttpRequest();
 forecastObject.open ("GET","//api.openweathermap.org/data/2.5/forecast?id=5604473&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1", true );
 forecastObject.send();
 forecastObject.onload = function() {
-  let forecastInfo = JSON.parse(forecastObjec.responseText);
+  let forecastInfo = JSON.parse(forecastObject.responseText);
   console.log(forecastInfo);
 
   var forecastItems = forecastObject.list;
