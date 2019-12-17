cf9513b8be058678b2a0caef27ed211839b50177
RitaBar <bar17055@byui.edu>
2019-12-17 16:16:18 -0500

    commit Final Project

---
+++ Final Project/js/templedata.js
@@ -0,0 +1,72 @@
+const requestURL = "/json/temples.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+
+    var temple = document.getElementById("boston").innerHTML;
+      if (temple.includes("Boston")) {
+      templeName = "Boston Massachusetts"
+    }
+
+    /*
+
+
+    var temple = document.getElementById("hartford").innerHTML;
+    if (temple.includes("Hartford")) {
+      templeName = "Hartford Connecticut"
+    }
+
+    var temple = document.getElementById("manhattan").innerHTML;
+      if (temple.includes("Manhattan")) {
+      templeName = "Manhattan New York"
+    }
+
+    var temple = document.getElementById("palmyra").innerHTML;
+      if (temple.includes("Palmyra")) {
+      templeName = "Palmyra New York"
+    }
+
+    */
+    var closed = jsonObject['temples'];
+
+    for (let i = 0; i < closed.length; i++) {
+      if (closed[i].name == templeName) {
+        console.log(templeName)
+
+        let data = document.createElement('section');
+        let h3 = document.createElement('h3');
+
+        h3.textContent = templeName + " Temple Closures for 2019";
+
+        function makeUL(array) {
+          //Create the list element;
+          var list = document.createElement('ul');
+
+          for (var j = 0; j < ((closed[i].closed.length)); j++) {
+
+            //Create the list item;
+            var item = document.createElement('li');
+
+            //set its contents;
+            item.appendChild(document.createTextNode(closed[i].closed[j]));
+
+            //Add it to the list:
+            list.appendChild(item);
+          }
+
+          //Finally, return the constructed list:
+          return list;
+
+        }
+        data.appendChild(h3);
+        //data.appendChild(list)
+
+        document.querySelector('div.closed').appendChild(data);
+        document.querySelector('div.closed').appendChild(makeUL());
+
+      }
+    }
+  })
---
+++ Final Project/js/weatherapi.js
@@ -0,0 +1,34 @@
+
+
+var weatherapi = "//api.openweathermap.org/data/2.5/weather?id=4834272&temp&units=imperial&APPID=35288c8a9bf8fbe38d102b914f4bd7b1";
+
+
+
+const weatherObject = new XMLHttpRequest();
+weatherObject.open ("GET",weatherapi, true );
+weatherObject.send();
+weatherObject.onload = function() {
+  let weatherInfo = JSON.parse(weatherObject.responseText);
+  console.log(weatherInfo);
+
+  document.getElementById('currentCondition').innerHTML = weatherInfo.weather[0].main;
+  document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+  document.getElementById('humidity').innerHTML= weatherInfo.main.humidity;
+  document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
+ 
+  function calcWindChill() {
+    var temp = parseFloat(document.getElementById('currentTemp').textContent);
+    var speed = parseFloat(document.getElementById('windSpeed').textContent);
+  
+    windchill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));
+  
+    if (windchill <= 50 && speed > 3) {
+      document.getElementById('wChill').textContent = windchill.toFixed(0) + "\xB0F";
+    } else {
+      document.getElementById('wChill').textContent = "NA";
+    }
+  }
+  calcWindChill();
+
+} // end of onload
+
